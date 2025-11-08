b2952855251655458518b0347d6eaaebb15c8f4d
unknown <jokerp2025@gmail.com>
2025-11-08 18:50:41 +0330

    fix: use Render port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`🌍 سرور آنلاین شد در پورت ${PORT}`);
+  console.log(`✅ سرور آنلاین شد در پورت ${PORT}`);
 });
